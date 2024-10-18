10ae4e59fa69e75972aec92e9dccfbe0773af9ff
Febialia <febi.223040059@mail.unpas.ac.id>
2024-10-18 16:06:28 +0700

    Kelar OOP MVC pusing

---
+++ OOP/13.AbstractClass-1/AbstractClaaa.php
@@ -0,0 +1,7 @@
+<?php 
+// sebuah kelas yang tidak dapat di instansiasi
+// mengidentifikasi interface untuk kelas lain yang menjadi turunan
+// sebagai kerangka dasar untuk kelas turunan
+// memiliki minimal 1 method abstrak
+
+?>
